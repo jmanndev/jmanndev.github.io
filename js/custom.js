00daaef7f2d89148acb54af3b0194961a03dde97
jmanndev <jmanndev@gmail.com>
2017-06-16 15:43:22 +1000

    Add contact form

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$('form#contact_form').validate({
+  messages: { },
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/jmanndev@gmail.com",
+      method: "POST",
+      data: $(form).serialize(),
+      dataType: "json",
+      success: function(data) {
+          $("form#contact_form :input").prop("disabled", true);
+          $('#thanks').show();
+      }
+    });
+    return false;
+  }
+});
